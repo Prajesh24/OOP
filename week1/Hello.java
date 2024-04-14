4e9106278e0fb87a47f139cef437c21163244d48
Prajesh24 <shresthaprajesh123@gmail.com>
2024-04-14 12:34:03 +0545

    week1 task1 all

--- week1/Hello.java
+++ week1/Hello.java
@@ -20,7 +20,33 @@ public class Hello{
         //     instead of " !
         //     '' is not the same as "
 
-        System.out.print("A \"quoted\" Sting is\n\'much\' better if you learn\nthe rules of \"escape sequences.\"");
+        System.out.print("A \"quoted\" Sting is\n\'much\' better if you learn\nthe rules of \"escape sequences.\"\nAlso, \"\" represents an empty\nString. Don't forget: use \\\"\ninstead of \" !\n'' is not the same as \"");
+
+        // 4. WAP that prints the following pattern:
+        // *
+        // **
+        // ***
+        // ****
+
+        System.out.print("\n*\n**\n***\n****");
+
+        // 5. WAP prints the following pattern:
+        // *********
+        // *********
+        // *********
+        // *********
+         System.out.print("\n*********\n*********\n*********\n*********");
+
+//          6. WAP that prints the following output:
+// A well-formed Java program has a main method with { and } braces.
+// A System.out.println statement has ( and ) and usually a String that starts and ends with a " character.
+// (But we type \" instead!)
+// What is the difference between
+// a ' and a "?  Or between a " and a \"?
+// One is what we see when we're typing our program. The other is what appears on the "console."
+
+
+        System.out.println("\nA well-formed Java program has a main method with { and } \nbraces.\n\nA System.out.println statement has ( and ) and usually a String that starts and ends with a \" character.\n(But we type \\\" instead!)\n\nWhat is the difference between\na \' and a \"?  Or between a \" and a \\\"?\n\nOne is what we see when we're typing our program. The other is what\nappears on the \"console.\"");
 
 
 
